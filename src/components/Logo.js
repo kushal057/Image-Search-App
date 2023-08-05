9c1f5884312743d75497de1b82f5233cd128b788
Kushal Tiwari <tiwarikushal057@gmail.com>
2023-08-06 00:59:55 +0545

    style: Change <span> tags into <p> tags for block display

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,8 @@ import styles from "./Logo.module.css"
 export default function Logo ({title, description}) {
     return (
         <div className={styles.logo}>
-            <p><span className={styles.title}>{title}</span> <br/> <span className={styles.description}>{description}</span></p>
+           <p className={styles.title}>{title}</p>
+           <p className={styles.description}>{description}</p>
         </div>
     )
 }
