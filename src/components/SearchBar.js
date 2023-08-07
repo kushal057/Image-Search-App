931a1bf30c4a74894287fca0a32219884e17bb11
Kushal Tiwari <tiwarikushal057@gmail.com>
2023-08-07 21:51:21 +0545

    feat: Make searchBar component functional for searching

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,13 @@
+import {useState, react} from 'react'
 import styles from "./SearchBar.module.css"
 import search_icon from "../assets/icons/search_icon.svg"
 
-export default function SearchBar() {
+export default function SearchBar({searchInput, handleSearchInput, handleReloadImages}) {
+
     return (
         <div className={styles.searchBar}>
-            <input type="text"/>
-            <img src={search_icon} alt="search"/>
+            <input type="text" value={searchInput} onChange={handleSearchInput}/>
+            <button onClick={handleReloadImages}><img src={search_icon} alt="search" /></button>
         </div>
     )
 }
